9d1f999ff075aa688c9afb98a4b0727eb51b106b
jordiprats <jordi.prats@gmail.com>
2018-02-03 12:53:59 +0100

    typo

--- testbot.py
+++ testbot.py
@@ -30,7 +30,7 @@ def ranking(bot, update):
     statsdb = config.get('bot', 'statsfile')
     statsconn = create_connection(statsdb)
     statscur = statsconn.cursor()
-    statscur.execute("SELECT id,id_user,ok,failed,display_name FROM stats ORDER BY ok DESC LIMIT 10", (user_id,))
+    statscur.execute("SELECT id,id_user,ok,failed,display_name FROM stats ORDER BY ok DESC LIMIT 10")
 
     users = statscur.fetchall()
 
